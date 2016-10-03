60db3e27ab867f5e706f29f31d95010cd84a0f01
vsvprasad <vsvprasad108@gmail.com>
2016-10-03 16:42:50 +0530

    MinCostPath done :+1:

---
+++ src/DynamicProgramming/MinCostPath.java
@@ -0,0 +1,170 @@
+package DynamicProgramming;
+
+import java.util.Scanner;
+
+public class MinCostPath {
+	static Scanner s = new Scanner(System.in);
+
+	public static int getNextInt() {
+		return Integer.parseInt(s.nextLine());
+	}
+
+	public static long getNextLong() {
+		return Long.parseLong(s.nextLine());
+	}
+
+	public static int[] getNextIntegerArray() {
+		String line = s.nextLine();
+		String[] tokens = line.split(" ");
+		int[] input = new int[tokens.length];
+		for (int i = 0; i < tokens.length; ++i) {
+			input[i] = Integer.parseInt(tokens[i]);
+		}
+		return input;
+	}
+
+	public static long[] getNextLongArray() {
+		String line = s.nextLine();
+		String[] tokens = line.split(" ");
+		long[] input = new long[tokens.length];
+		for (int i = 0; i < tokens.length; ++i) {
+			input[i] = Long.parseLong(tokens[i]);
+		}
+		return input;
+	}
+
+	public static String getNextLine() {
+		return s.nextLine();
+	}
+
+	public static String[] getNextStringArray() {
+		String line = s.nextLine();
+		String[] tokens = line.split(" ");
+		return tokens;
+	}
+
+	public static void printArray(Object[] objs) {
+		for (Object o : objs) {
+			System.out.println(o);
+		}
+	}
+
+	public static int max(int i, int j) {
+		if (i > j) {
+			return i;
+		}
+		return j;
+	}
+
+	public static String max(String s1, String s2) {
+		if (s1.length() > s2.length()) {
+			return s1;
+		}
+		return s2;
+	}
+
+	public static int max(int i, int j, int k) {
+		if (i > j) {
+			if (i > k) {
+				return i;
+			} else {
+				return k;
+			}
+		}
+		if (j > k) {
+			return j;
+		} else {
+			return k;
+		}
+
+	}
+
+	public static String max(String s1, String s2, String s3) {
+		int i = s1.length(), j = s2.length(), k = s3.length();
+		if (i > j) {
+			if (i > k) {
+				return s1;
+			} else {
+				return s3;
+			}
+		}
+		if (j > k) {
+			return s2;
+		} else {
+			return s3;
+		}
+	}
+
+	public static int min(int i, int j) {
+		if (i < j) {
+			return i;
+		}
+		return j;
+	}
+
+	public static String min(String s1, String s2) {
+		if (s1.length() < s2.length()) {
+			return s1;
+		}
+		return s2;
+	}
+
+	public static int min(int i, int j, int k) {
+		if (i < j) {
+			if (i < k) {
+				return i;
+			} else {
+				return k;
+			}
+		}
+		if (j < k) {
+			return j;
+		} else {
+			return k;
+		}
+
+	}
+
+	public static String min(String s1, String s2, String s3) {
+		int i = s1.length(), j = s2.length(), k = s3.length();
+		if (i < j) {
+			if (i < k) {
+				return s1;
+			} else {
+				return s3;
+			}
+		}
+		if (j < k) {
+			return s2;
+		} else {
+			return s3;
+		}
+	}
+
+	public static void main(String[] args) {
+		int cost[][] = { {1, 2, 3},
+                {4, 8, 2},
+                {1, 5, 3} };
+		int[][] optimizedCosts = new int[3][3];
+		//starting point is (0,0)
+		
+		int x = getNextInt();
+		int y = getNextInt();
+		for(int i=0; i<=2;++i){
+			for(int j=0; j<=2;++j){
+				if(i==0 && j==0){
+					optimizedCosts[i][j] = cost[i][j];
+				}
+				else if(i==0){
+					optimizedCosts[0][j] = optimizedCosts[0][j-1] + cost[0][j];
+				}
+				else if(j==0){
+					optimizedCosts[i][0] = optimizedCosts[i-1][0] + cost[i][0];
+				}else{
+					optimizedCosts[i][j] = cost[i][j] + min(optimizedCosts[i-1][j-1], optimizedCosts[i-1][j], optimizedCosts[i][j-1]);
+				}
+			}
+		}
+		System.out.println(optimizedCosts[x][y]);
+	}
+}
