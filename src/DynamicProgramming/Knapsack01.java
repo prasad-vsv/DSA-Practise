0359e5ab4875b1fc6303577c8c8d6eaca60e6453
vsvprasad <vsvprasad108@gmail.com>
2016-10-10 12:50:36 +0530

    Knapsack 0-1 solved :+1:

---
+++ src/DynamicProgramming/Knapsack01.java
@@ -0,0 +1,169 @@
+package DynamicProgramming;
+
+import java.util.Scanner;
+
+public class Knapsack01 {
+	static Scanner s = new Scanner(System.in);
+
+	public static int getNextInt() {
+		return Integer.parseInt(s.nextLine());
+	}
+
+	public static long getNextLong() {
+		return Long.parseLong(s.nextLine());
+	}
+
+	public static int[] getNextIntegerArray() {
+		String line = s.nextLine();
+		String[] tokens = line.split(" ");
+		int[] input = new int[tokens.length];
+		for (int i = 0; i < tokens.length; ++i) {
+			input[i] = Integer.parseInt(tokens[i]);
+		}
+		return input;
+	}
+
+	public static long[] getNextLongArray() {
+		String line = s.nextLine();
+		String[] tokens = line.split(" ");
+		long[] input = new long[tokens.length];
+		for (int i = 0; i < tokens.length; ++i) {
+			input[i] = Long.parseLong(tokens[i]);
+		}
+		return input;
+	}
+
+	public static String getNextLine() {
+		return s.nextLine();
+	}
+
+	public static String[] getNextStringArray() {
+		String line = s.nextLine();
+		String[] tokens = line.split(" ");
+		return tokens;
+	}
+
+	public static void printArray(Object[] objs) {
+		for (Object o : objs) {
+			System.out.println(o);
+		}
+	}
+
+	public static int max(int i, int j) {
+		if (i > j) {
+			return i;
+		}
+		return j;
+	}
+
+	public static String max(String s1, String s2) {
+		if (s1.length() > s2.length()) {
+			return s1;
+		}
+		return s2;
+	}
+
+	public static int max(int i, int j, int k) {
+		if (i > j) {
+			if (i > k) {
+				return i;
+			} else {
+				return k;
+			}
+		}
+		if (j > k) {
+			return j;
+		} else {
+			return k;
+		}
+
+	}
+
+	public static String max(String s1, String s2, String s3) {
+		int i = s1.length(), j = s2.length(), k = s3.length();
+		if (i > j) {
+			if (i > k) {
+				return s1;
+			} else {
+				return s3;
+			}
+		}
+		if (j > k) {
+			return s2;
+		} else {
+			return s3;
+		}
+	}
+
+	public static int min(int i, int j) {
+		if (i < j) {
+			return i;
+		}
+		return j;
+	}
+
+	public static String min(String s1, String s2) {
+		if (s1.length() < s2.length()) {
+			return s1;
+		}
+		return s2;
+	}
+
+	public static int min(int i, int j, int k) {
+		if (i < j) {
+			if (i < k) {
+				return i;
+			} else {
+				return k;
+			}
+		}
+		if (j < k) {
+			return j;
+		} else {
+			return k;
+		}
+
+	}
+
+	public static String min(String s1, String s2, String s3) {
+		int i = s1.length(), j = s2.length(), k = s3.length();
+		if (i < j) {
+			if (i < k) {
+				return s1;
+			} else {
+				return s3;
+			}
+		}
+		if (j < k) {
+			return s2;
+		} else {
+			return s3;
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] values = getNextIntegerArray();
+		int[] weights = getNextIntegerArray();
+		int capacity = getNextInt();
+		
+		int[][] matrix = new int[values.length+1][capacity +1];
+		
+		for(int i=0; i<=values.length;++i){
+			for(int j=0; j<=capacity;++j){
+				if(i==0 || j==0 ){
+					//that means that either there are no elements(i==0) or there is no capacity to consider
+					matrix[i][j] = 0;
+				}else if (weights[i-1]>j){
+					//last one cannot be considered into the knapsack because its weight is greater than the total capacity(for this iteration)
+					matrix[i][j] = matrix[i-1][j];
+				}else{
+					//two cases:
+					//include the latest element => weight reduces by w[i] and value increases by v[i]
+					//exclude the latest element => both weight and value remain the same
+					matrix[i][j] = max(values[i-1] + matrix[i-1][j-weights[i-1]], matrix[i-1][j]);
+				}
+			}
+		}
+		System.out.println(matrix[values.length][capacity]);
+	}
+}
