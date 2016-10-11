92c1f372ac53d4c6a5864e7e3aaa9b0d68fea85d
vsvprasad <vsvprasad108@gmail.com>
2016-10-11 16:33:58 +0530

    WaysToReachNthStair completed :+1:

---
+++ src/DynamicProgramming/WaysToReachNthStair.java
@@ -0,0 +1,158 @@
+package DynamicProgramming;
+
+import java.util.Scanner;
+
+public class WaysToReachNthStair {
+	static Scanner s = new Scanner(System.in);
+
+	public static int getNextInt() {
+		return Integer.parseInt(s.nextLine());
+	}
+
+	public static long getNextLong() {
+		return Long.parseLong(s.nextLine());
+	}
+
+	public static int[] getNextIntegerArray() {
+		String line = s.nextLine();
+		String[] tokens = line.split(" ");
+		int[] input = new int[tokens.length];
+		for (int i = 0; i < tokens.length; ++i) {
+			input[i] = Integer.parseInt(tokens[i]);
+		}
+		return input;
+	}
+
+	public static long[] getNextLongArray() {
+		String line = s.nextLine();
+		String[] tokens = line.split(" ");
+		long[] input = new long[tokens.length];
+		for (int i = 0; i < tokens.length; ++i) {
+			input[i] = Long.parseLong(tokens[i]);
+		}
+		return input;
+	}
+
+	public static String getNextLine() {
+		return s.nextLine();
+	}
+
+	public static String[] getNextStringArray() {
+		String line = s.nextLine();
+		String[] tokens = line.split(" ");
+		return tokens;
+	}
+
+	public static void printArray(Object[] objs) {
+		for (Object o : objs) {
+			System.out.println(o);
+		}
+	}
+
+	public static int max(int i, int j) {
+		if (i > j) {
+			return i;
+		}
+		return j;
+	}
+
+	public static String max(String s1, String s2) {
+		if (s1.length() > s2.length()) {
+			return s1;
+		}
+		return s2;
+	}
+
+	public static int max(int i, int j, int k) {
+		if (i > j) {
+			if (i > k) {
+				return i;
+			} else {
+				return k;
+			}
+		}
+		if (j > k) {
+			return j;
+		} else {
+			return k;
+		}
+
+	}
+
+	public static String max(String s1, String s2, String s3) {
+		int i = s1.length(), j = s2.length(), k = s3.length();
+		if (i > j) {
+			if (i > k) {
+				return s1;
+			} else {
+				return s3;
+			}
+		}
+		if (j > k) {
+			return s2;
+		} else {
+			return s3;
+		}
+	}
+
+	public static int min(int i, int j) {
+		if (i < j) {
+			return i;
+		}
+		return j;
+	}
+
+	public static String min(String s1, String s2) {
+		if (s1.length() < s2.length()) {
+			return s1;
+		}
+		return s2;
+	}
+
+	public static int min(int i, int j, int k) {
+		if (i < j) {
+			if (i < k) {
+				return i;
+			} else {
+				return k;
+			}
+		}
+		if (j < k) {
+			return j;
+		} else {
+			return k;
+		}
+
+	}
+
+	public static String min(String s1, String s2, String s3) {
+		int i = s1.length(), j = s2.length(), k = s3.length();
+		if (i < j) {
+			if (i < k) {
+				return s1;
+			} else {
+				return s3;
+			}
+		}
+		if (j < k) {
+			return s2;
+		} else {
+			return s3;
+		}
+	}
+
+	public static void main(String[] args) {
+		int steps = getNextInt();
+		int[] answers = new int[steps + 1];
+		answers[0] = 0; // if there are no steps to climb
+		answers[1] = 1; // by climbing one step
+		answers[2] = 2; // either by climbing one step at a time or two steps at once
+		for(int i=3; i<=steps; ++i){
+			//there are only two possible ways to reach a particular step.
+			//either by taking one step from the previous step
+			//or by taking two steps at once from the step before the previous step
+			answers[i] = answers[i-1] + answers[i-2];
+		}
+		System.out.println(answers[steps]);
+	}
+}
