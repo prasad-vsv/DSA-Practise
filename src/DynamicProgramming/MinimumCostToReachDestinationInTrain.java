8fc989776f5fc019cd724442ae9523a6269ef761
vsvprasad <vsvprasad108@gmail.com>
2016-10-11 23:23:09 +0530

    MinimumCostToReachDestinationInTrain done :+1:

---
+++ src/DynamicProgramming/MinimumCostToReachDestinationInTrain.java
@@ -0,0 +1,192 @@
+package DynamicProgramming;
+
+import java.util.Scanner;
+
+public class MinimumCostToReachDestinationInTrain {
+	static Scanner s = new Scanner(System.in);
+
+	public static int getNextInt() {
+		return Integer.parseInt(s.nextLine());
+	}
+
+	public static long getNextLong() {
+		return Long.parseLong(s.nextLine());
+	}
+
+	public static int[] getNextIntegerArray() {
+		String line = s.nextLine();
+		String[] tokens = line.split(" ");
+		int[] input = new int[tokens.length];
+		for (int i = 0; i < tokens.length; ++i) {
+			input[i] = Integer.parseInt(tokens[i]);
+		}
+		return input;
+	}
+
+	public static long[] getNextLongArray() {
+		String line = s.nextLine();
+		String[] tokens = line.split(" ");
+		long[] input = new long[tokens.length];
+		for (int i = 0; i < tokens.length; ++i) {
+			input[i] = Long.parseLong(tokens[i]);
+		}
+		return input;
+	}
+
+	public static String getNextLine() {
+		return s.nextLine();
+	}
+
+	public static String[] getNextStringArray() {
+		String line = s.nextLine();
+		String[] tokens = line.split(" ");
+		return tokens;
+	}
+
+	public static void printArray(Object[] objs) {
+		for (Object o : objs) {
+			System.out.println(o);
+		}
+	}
+
+	public static int max(int i, int j) {
+		if (i > j) {
+			return i;
+		}
+		return j;
+	}
+
+	public static String max(String s1, String s2) {
+		if (s1.length() > s2.length()) {
+			return s1;
+		}
+		return s2;
+	}
+
+	public static int max(int i, int j, int k) {
+		if (i > j) {
+			if (i > k) {
+				return i;
+			} else {
+				return k;
+			}
+		}
+		if (j > k) {
+			return j;
+		} else {
+			return k;
+		}
+
+	}
+
+	public static String max(String s1, String s2, String s3) {
+		int i = s1.length(), j = s2.length(), k = s3.length();
+		if (i > j) {
+			if (i > k) {
+				return s1;
+			} else {
+				return s3;
+			}
+		}
+		if (j > k) {
+			return s2;
+		} else {
+			return s3;
+		}
+	}
+
+	public static int min(int i, int j) {
+		if (i < j) {
+			return i;
+		}
+		return j;
+	}
+
+	public static String min(String s1, String s2) {
+		if (s1.length() < s2.length()) {
+			return s1;
+		}
+		return s2;
+	}
+
+	public static int min(int i, int j, int k) {
+		if (i < j) {
+			if (i < k) {
+				return i;
+			} else {
+				return k;
+			}
+		}
+		if (j < k) {
+			return j;
+		} else {
+			return k;
+		}
+
+	}
+
+	public static String min(String s1, String s2, String s3) {
+		int i = s1.length(), j = s2.length(), k = s3.length();
+		if (i < j) {
+			if (i < k) {
+				return s1;
+			} else {
+				return s3;
+			}
+		}
+		if (j < k) {
+			return s2;
+		} else {
+			return s3;
+		}
+	}
+
+	public static void main(String[] args) {
+		// http://www.geeksforgeeks.org/find-the-minimum-cost-to-reach-a-destination-where-every-station-is-connected-in-one-direction/
+		int[][] costs = {{0,15,80,90},{Integer.MAX_VALUE,0,40,50},{Integer.MAX_VALUE,Integer.MAX_VALUE,0,70},{Integer.MAX_VALUE,Integer.MAX_VALUE,Integer.MAX_VALUE,0}};
+		calculateMinCost(costs,4); //second arguement is the total number of stations
+	}
+
+	private static void calculateMinCost(int[][] costs, int stations) {
+		int[] minCosts = new int[stations];
+		for(int i=0; i<stations; ++i){
+			if(i==0){
+				minCosts[0] = 0; //base condition
+				continue;
+			}
+			int min = Integer.MAX_VALUE;
+			for(int j=0;j<i;++j){ //iterating over all the stations before this station and finding the min cost to finally reach here
+				int temp = costs[j][i]+ minCosts[j]; //min cost of reaching till that point + cost of coming from that point till the destination
+				if(temp<min){
+					min = temp;
+				}
+			}
+			minCosts[i] = min;
+		}
+		System.out.println(minCosts[stations-1]);
+	}
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
