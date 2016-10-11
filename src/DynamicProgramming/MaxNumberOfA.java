27e4ec6f9d9e02c9b405fcce07af0cfcecb62f42
vsvprasad <vsvprasad108@gmail.com>
2016-10-11 22:47:00 +0530

    Maximum number of A's is done

---
+++ src/DynamicProgramming/MaxNumberOfA.java
@@ -0,0 +1,170 @@
+package DynamicProgramming;
+
+import java.util.Scanner;
+
+public class MaxNumberOfA {
+	static Scanner s = new Scanner(System.in);
+
+	public static int getNextInt() {
+		return Integer.parseInt(s.nextLine());
+	}
+
+	public static long getNextLong() {
+		return Long.parseLong(s.nextLine());
+	}
+
+	public static int[] getNextIntegerArray() {
+		String line = s.nextLine();
+		String[] tokens = line.split(" ");
+		int[] input = new int[tokens.length];
+		for (int i = 0; i < tokens.length; ++i) {
+			input[i] = Integer.parseInt(tokens[i]);
+		}
+		return input;
+	}
+
+	public static long[] getNextLongArray() {
+		String line = s.nextLine();
+		String[] tokens = line.split(" ");
+		long[] input = new long[tokens.length];
+		for (int i = 0; i < tokens.length; ++i) {
+			input[i] = Long.parseLong(tokens[i]);
+		}
+		return input;
+	}
+
+	public static String getNextLine() {
+		return s.nextLine();
+	}
+
+	public static String[] getNextStringArray() {
+		String line = s.nextLine();
+		String[] tokens = line.split(" ");
+		return tokens;
+	}
+
+	public static void printArray(Object[] objs) {
+		for (Object o : objs) {
+			System.out.println(o);
+		}
+	}
+
+	public static int max(int i, int j) {
+		if (i > j) {
+			return i;
+		}
+		return j;
+	}
+
+	public static String max(String s1, String s2) {
+		if (s1.length() > s2.length()) {
+			return s1;
+		}
+		return s2;
+	}
+
+	public static int max(int i, int j, int k) {
+		if (i > j) {
+			if (i > k) {
+				return i;
+			} else {
+				return k;
+			}
+		}
+		if (j > k) {
+			return j;
+		} else {
+			return k;
+		}
+
+	}
+
+	public static String max(String s1, String s2, String s3) {
+		int i = s1.length(), j = s2.length(), k = s3.length();
+		if (i > j) {
+			if (i > k) {
+				return s1;
+			} else {
+				return s3;
+			}
+		}
+		if (j > k) {
+			return s2;
+		} else {
+			return s3;
+		}
+	}
+
+	public static int min(int i, int j) {
+		if (i < j) {
+			return i;
+		}
+		return j;
+	}
+
+	public static String min(String s1, String s2) {
+		if (s1.length() < s2.length()) {
+			return s1;
+		}
+		return s2;
+	}
+
+	public static int min(int i, int j, int k) {
+		if (i < j) {
+			if (i < k) {
+				return i;
+			} else {
+				return k;
+			}
+		}
+		if (j < k) {
+			return j;
+		} else {
+			return k;
+		}
+
+	}
+
+	public static String min(String s1, String s2, String s3) {
+		int i = s1.length(), j = s2.length(), k = s3.length();
+		if (i < j) {
+			if (i < k) {
+				return s1;
+			} else {
+				return s3;
+			}
+		}
+		if (j < k) {
+			return s2;
+		} else {
+			return s3;
+		}
+	}
+
+	public static void main(String[] args) {
+		// http://www.geeksforgeeks.org/how-to-print-maximum-number-of-a-using-given-four-keys/
+		int count = getNextInt();
+		int[] cache = new int[count + 1];
+		cache[0] = 0;
+		cache[1] = 1;
+		cache[2] = 2;
+		cache[3] = 3;
+		int currentCache = 0;
+		for(int i=4; i<=count ; ++i){
+			int a = 2*cache[i-3]; //3 steps gone for ctrl+A, ctrl+C, ctrl +V and then the output doubles
+			int b = cache[i-1]+ currentCache; //simply paste the current cache without copying the whole screen, in which case it will be sum of exisitng string on screen and the currentCache
+			int c = cache[i-1]+1;
+			if(a== max(a,b,c)){
+				currentCache = cache[i-3]; //because we have copied cache[i-3], now we update currentCache to that
+				cache[i] =a;
+			}else if(b == max(a,b,c)){
+				cache[i]= b;
+				//current cache remains the same
+			}else{
+				cache[i] = c;
+				//current cache remains the same
+			}
+		}
+		System.out.println(cache[count]);
+	}
+}
