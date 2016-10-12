6c317fefd378cbe98d72be017aad8cc464a704bc
vsvprasad <vsvprasad108@gmail.com>
2016-10-13 00:48:34 +0530

    MaximumSumWithNoAdjacentElements done :+1:

---
+++ src/DynamicProgramming/MaximumSumWithNoAdjacentElements.java
@@ -0,0 +1,171 @@
+package DynamicProgramming;
+
+import java.util.Scanner;
+
+public class MaximumSumWithNoAdjacentElements {
+	static Scanner s = new Scanner(System.in);
+
+	public static int getNextInt() {
+		return Integer.parseInt(s.nextLine());
+	}
+
+	public static long getNextLong() {
+		return Long.parseLong(s.nextLine());
+	}
+
+	public static int[] getNextIntegerArray() {
+		String line = s.nextLine();
+		String[] tokens = line.split(" ");
+		int[] input = new int[tokens.length];
+		for (int i = 0; i < tokens.length; ++i) {
+			input[i] = Integer.parseInt(tokens[i]);
+		}
+		return input;
+	}
+
+	public static long[] getNextLongArray() {
+		String line = s.nextLine();
+		String[] tokens = line.split(" ");
+		long[] input = new long[tokens.length];
+		for (int i = 0; i < tokens.length; ++i) {
+			input[i] = Long.parseLong(tokens[i]);
+		}
+		return input;
+	}
+
+	public static String getNextLine() {
+		return s.nextLine();
+	}
+
+	public static String[] getNextStringArray() {
+		String line = s.nextLine();
+		String[] tokens = line.split(" ");
+		return tokens;
+	}
+
+	public static void printArray(Object[] objs) {
+		for (Object o : objs) {
+			System.out.println(o);
+		}
+	}
+
+	public static int max(int i, int j) {
+		if (i > j) {
+			return i;
+		}
+		return j;
+	}
+
+	public static String max(String s1, String s2) {
+		if (s1.length() > s2.length()) {
+			return s1;
+		}
+		return s2;
+	}
+
+	public static int max(int i, int j, int k) {
+		if (i > j) {
+			if (i > k) {
+				return i;
+			} else {
+				return k;
+			}
+		}
+		if (j > k) {
+			return j;
+		} else {
+			return k;
+		}
+
+	}
+
+	public static String max(String s1, String s2, String s3) {
+		int i = s1.length(), j = s2.length(), k = s3.length();
+		if (i > j) {
+			if (i > k) {
+				return s1;
+			} else {
+				return s3;
+			}
+		}
+		if (j > k) {
+			return s2;
+		} else {
+			return s3;
+		}
+	}
+
+	public static int min(int i, int j) {
+		if (i < j) {
+			return i;
+		}
+		return j;
+	}
+
+	public static String min(String s1, String s2) {
+		if (s1.length() < s2.length()) {
+			return s1;
+		}
+		return s2;
+	}
+
+	public static int min(int i, int j, int k) {
+		if (i < j) {
+			if (i < k) {
+				return i;
+			} else {
+				return k;
+			}
+		}
+		if (j < k) {
+			return j;
+		} else {
+			return k;
+		}
+
+	}
+
+	public static String min(String s1, String s2, String s3) {
+		int i = s1.length(), j = s2.length(), k = s3.length();
+		if (i < j) {
+			if (i < k) {
+				return s1;
+			} else {
+				return s3;
+			}
+		}
+		if (j < k) {
+			return s2;
+		} else {
+			return s3;
+		}
+	}
+
+	public static void main(String[] args) {
+		//http://www.geeksforgeeks.org/maximum-sum-such-that-no-two-elements-are-adjacent/
+		int [] input = getNextIntegerArray();
+		int[] sums = new int[input.length];
+		for(int i=0; i< input.length; ++i){
+			//bottom up such that we calculate sums at every point assuming that we include that element
+			//finally we take max out of sums
+			if(i==0){
+				sums[i] = input[i];
+				continue;
+			}
+			int max = Integer.MIN_VALUE;
+			for(int j=0; j<i-1; ++j){
+				if(max < (sums[j]+input[i])){
+					max = sums[j]+input[i];
+				}
+			}
+			sums[i] = max;
+		}
+		int global = Integer.MIN_VALUE;
+		for(int i=0; i<sums.length; ++i){
+			if(sums[i] > global){
+				global = sums[i];
+			}
+		}
+		System.out.println(global);
+	}
+}
