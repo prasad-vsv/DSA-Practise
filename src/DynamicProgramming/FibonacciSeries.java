66e327bdddbd2e0bef16377d580651f2e30924ab
prasad-vsv <prasad-vsv@users.noreply.github.com>
2017-06-15 08:55:02 +0530

    Adding Fibonacci series

---
+++ src/DynamicProgramming/FibonacciSeries.java
@@ -0,0 +1,167 @@
+package DynamicProgramming;
+
+import java.util.*;
+
+public class FibonacciSeries {
+	static Scanner s = new Scanner(System.in);
+
+	public static int getNextInt() {
+		return Integer.parseInt(s.nextLine());
+	}
+
+	public static long getNextLong() {
+		return Long.parseLong(s.nextLine());
+	}
+
+	public static int[] getNextIntegerArray() {
+		String line = s.nextLine();
+		String[] tokens = line.split(" ");
+		int[] input = new int[tokens.length];
+		for (int i = 0; i < tokens.length; ++i) {
+			input[i] = Integer.parseInt(tokens[i]);
+		}
+		return input;
+	}
+
+	public static long[] getNextLongArray() {
+		String line = s.nextLine();
+		String[] tokens = line.split(" ");
+		long[] input = new long[tokens.length];
+		for (int i = 0; i < tokens.length; ++i) {
+			input[i] = Long.parseLong(tokens[i]);
+		}
+		return input;
+	}
+
+	public static String getNextLine() {
+		return s.nextLine();
+	}
+
+	public static String[] getNextStringArray() {
+		String line = s.nextLine();
+		String[] tokens = line.split(" ");
+		return tokens;
+	}
+
+	public static void printArray(Object[] objs) {
+		for (Object o : objs) {
+			System.out.println(o);
+		}
+	}
+
+	public static int max(int i, int j) {
+		if (i > j) {
+			return i;
+		}
+		return j;
+	}
+
+	public static String max(String s1, String s2) {
+		if (s1.length() > s2.length()) {
+			return s1;
+		}
+		return s2;
+	}
+
+	public static int max(int i, int j, int k) {
+		if (i > j) {
+			if (i > k) {
+				return i;
+			} else {
+				return k;
+			}
+		}
+		if (j > k) {
+			return j;
+		} else {
+			return k;
+		}
+
+	}
+
+	public static String max(String s1, String s2, String s3) {
+		int i = s1.length(), j = s2.length(), k = s3.length();
+		if (i > j) {
+			if (i > k) {
+				return s1;
+			} else {
+				return s3;
+			}
+		}
+		if (j > k) {
+			return s2;
+		} else {
+			return s3;
+		}
+	}
+
+	public static int min(int i, int j) {
+		if (i < j) {
+			return i;
+		}
+		return j;
+	}
+
+	public static String min(String s1, String s2) {
+		if (s1.length() < s2.length()) {
+			return s1;
+		}
+		return s2;
+	}
+
+	public static int min(int i, int j, int k) {
+		if (i < j) {
+			if (i < k) {
+				return i;
+			} else {
+				return k;
+			}
+		}
+		if (j < k) {
+			return j;
+		} else {
+			return k;
+		}
+
+	}
+
+	public static String min(String s1, String s2, String s3) {
+		int i = s1.length(), j = s2.length(), k = s3.length();
+		if (i < j) {
+			if (i < k) {
+				return s1;
+			} else {
+				return s3;
+			}
+		}
+		if (j < k) {
+			return s2;
+		} else {
+			return s3;
+		}
+	}
+
+	public static void main(String[] args) {
+		// code
+		Scanner s = new Scanner(System.in);
+		int n = Integer.parseInt(s.nextLine());
+		for (int i = 0; i < n; ++i) {
+			System.out.println(fibonacci(Integer.parseInt(s.nextLine())));
+		}
+
+	}
+
+	public static int fibonacci(int n) {
+		int[] arr = new int[n + 1];
+		if (n == 0 || n == 1) {
+			return n;
+		}
+		// base cases
+		arr[0] = 0;
+		arr[1] = 1;
+		for (int i = 2; i <= n; ++i) {
+			arr[i] = (arr[i - 1] + arr[i - 2]) % 1000000007;
+		}
+		return arr[n];
+	}
+}
